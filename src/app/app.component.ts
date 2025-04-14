1792f193521d03ab3b73fcb8c499ce12eb15f158
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-14 23:28:36 +0300

    structured page view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ShelfComponent } from './shelf/shelf.component';
 import { FavShelfComponent } from './fav-shelf/fav-shelf.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ShelfComponent, FavShelfComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ShelfComponent,
+    FavShelfComponent,
+    UserPanelComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
