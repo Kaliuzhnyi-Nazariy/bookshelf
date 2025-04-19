d9da4d8195292a11dc17335709fa3f0c9071c54a
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-19 21:51:01 +0300

    user panel logic and view

--- src/app/user-panel/user-panel.component.ts
+++ src/app/user-panel/user-panel.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SignUpModalComponent } from '../sign-up-modal/sign-up-modal.component';
+import { AuthService } from '../api';
+import { SignInModalComponent } from '../sign-in-modal/sign-in-modal.component';
 
 @Component({
   selector: 'app-user-panel',
-  imports: [],
+  imports: [SignUpModalComponent, SignInModalComponent],
   templateUrl: './user-panel.component.html',
   styleUrl: './user-panel.component.css',
 })
-export class UserPanelComponent {
-  public name: string = 'Vasya';
-  public email: string = 'vasya@mail.com';
+export class UserPanelComponent implements AfterViewInit, OnInit {
+  constructor(private dialog: MatDialog, private authService: AuthService) {}
+
+  isLoggedIn = false;
+  userName: string = '';
+  userEmail: string = '';
+
+  ngOnInit(): void {
+    this.authService.checkIfLogged()?.subscribe();
+    console.log(this.userName);
+  }
+
+  ngAfterViewInit(): void {
+    this.authService.authStatus.subscribe((status) => {
+      this.isLoggedIn = status;
+    });
+    this.authService.userData.subscribe((data) => {
+      console.log(data.name);
+      this.userName = data.name;
+      this.userEmail = data.email;
+    });
+  }
+
   public allAmountOfBooks: number = 17;
+
+  openSignUpModal(): void {
+    this.dialog.open(SignUpModalComponent, {
+      width: '300px',
+      minHeight: '410px',
+    });
+  }
+
+  openSignInModal(): void {
+    this.dialog.open(SignInModalComponent, {
+      width: '300px',
+      minHeight: '250px',
+    });
+  }
 }
