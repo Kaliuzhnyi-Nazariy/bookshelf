6eb8f87269fbe895b4e947ed66a405418857a8dd
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-20 22:19:17 +0300

    add button for logout

--- src/app/user-panel/user-panel.component.ts
+++ src/app/user-panel/user-panel.component.ts
@@ -1,24 +1,32 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SignUpModalComponent } from '../sign-up-modal/sign-up-modal.component';
-import { AuthService } from '../api';
+import { AuthService, UsersService } from '../api';
 import { SignInModalComponent } from '../sign-in-modal/sign-in-modal.component';
+import { DelModalComponent } from '../del-modal/del-modal.component';
+import { UpdModalComponent } from '../upd-modal/upd-modal.component';
 
 @Component({
   selector: 'app-user-panel',
-  imports: [SignUpModalComponent, SignInModalComponent],
+  imports: [SignUpModalComponent, SignInModalComponent, DelModalComponent],
   templateUrl: './user-panel.component.html',
   styleUrl: './user-panel.component.css',
 })
 export class UserPanelComponent implements AfterViewInit, OnInit {
-  constructor(private dialog: MatDialog, private authService: AuthService) {}
+  constructor(
+    private dialog: MatDialog,
+    private authService: AuthService,
+    private userService: UsersService
+  ) {}
 
   isLoggedIn = false;
   userName: string = '';
   userEmail: string = '';
 
+  errReqMessage = signal('');
+
   ngOnInit(): void {
-    this.authService.checkIfLogged()?.subscribe();
+    this.userService.checkIfLogged()?.subscribe();
     console.log(this.userName);
   }
 
@@ -27,7 +35,6 @@ export class UserPanelComponent implements AfterViewInit, OnInit {
       this.isLoggedIn = status;
     });
     this.authService.userData.subscribe((data) => {
-      console.log(data.name);
       this.userName = data.name;
       this.userEmail = data.email;
     });
@@ -46,6 +53,23 @@ export class UserPanelComponent implements AfterViewInit, OnInit {
     this.dialog.open(SignInModalComponent, {
       width: '300px',
       minHeight: '250px',
+    });
+  }
+
+  openUpdModal(): void {
+    this.dialog.open(UpdModalComponent, {
+      width: '350px',
+    });
+  }
+  openDelModal(): void {
+    this.dialog.open(DelModalComponent, {
+      width: '350px',
+    });
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => {},
     });
   }
 }
