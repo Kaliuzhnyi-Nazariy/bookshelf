7684b89cb737e2eb667ee40c218769652ea7fd3c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-21 12:02:26 +0300

    add delete modal function

--- src/app/del-modal/del-modal.component.ts
+++ src/app/del-modal/del-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { UsersService } from '../api';
 
 @Component({
   selector: 'app-del-modal',
@@ -8,9 +9,33 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrl: './del-modal.component.css',
 })
 export class DelModalComponent {
-  constructor(public dialog: MatDialogRef<DelModalComponent>) {}
+  isLoading = signal(false);
+  reqErrMessage = signal('');
+
+  constructor(
+    public dialog: MatDialogRef<DelModalComponent>,
+    private userService: UsersService
+  ) {}
 
   closeModal() {
     this.dialog.close();
+  }
+
+  deleteUser(): void {
+    this.isLoading.set(true);
+    try {
+      this.userService.deleteUser()?.subscribe({
+        next: () => {
+          this.isLoading.set(false);
+          this.dialog.close(); // Close the modal on success
+        },
+        error: (err) => {
+          this.isLoading.set(false);
+          this.reqErrMessage.set(err.error.message);
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
