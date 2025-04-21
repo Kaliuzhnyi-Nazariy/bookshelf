9bc437a71967f22489ea6936ff35747a96c773c9
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-21 21:56:29 +0300

    add services for receiving and posting books

--- src/app/api/book.service.ts
+++ src/app/api/book.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { accessToken } from './helper';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AddBook, Book } from '../dtos';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +15,24 @@ export class BookService {
   constructor() {}
 
   token: string | undefined = accessToken();
+
+  getBooks(): Observable<Book[]> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    };
+
+    return this.http.get<Book[]>(`${this.baseURL}`, { headers });
+  }
+
+  postBook(dto: AddBook): Observable<Book> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    };
+
+    return this.http.post<Book>(`${this.baseURL}`, dto, { headers });
+  }
 
   checkIfToken(): void {
     console.log(this.token);
