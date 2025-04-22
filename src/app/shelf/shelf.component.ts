33cb7a4e75278d9caff1139cae6354553aa3dfe2
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-22 12:31:08 +0300

    styles and some logic for shelf

--- src/app/shelf/shelf.component.ts
+++ src/app/shelf/shelf.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BookUpdModalComponent } from '../book-upd-modal/book-upd-modal.component';
+import { BookService } from '../api/book.service';
+import { AddBookModalComponent } from '../add-book-modal/add-book-modal.component';
 
 @Component({
   selector: 'app-shelf',
@@ -6,37 +10,61 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './shelf.component.html',
   styleUrl: './shelf.component.css',
 })
-export class ShelfComponent implements OnInit {
+export class ShelfComponent implements OnInit, AfterContentInit {
   public listForTest: string[] = [];
+  public chunkedList: string[][] = [];
   private columnAmount = 7;
-  public fillers: any[] = [];
+
+  constructor(public dialog: MatDialog, private bookService: BookService) {}
 
   ngOnInit(): void {
     this.listForTest = [
-      // 'name1',
-      // 'name2',
-      // 'name3',
-      // 'name4',
-      // 'name5',
-      // 'name6',
-      // 'name7',
-      // 'name8',
-      // 'name9',
-      // 'name10',
-      // 'name11',
-      // 'name21',
-      // 'name31',
-      // 'name41',
-      // 'name51',
-      // 'name61',
-      // 'name71',
-      // 'name81',
-      // 'name91',
-      // 'name101',
+      'name1',
+      'name2',
+      'name3',
+      'name4',
+      'name5',
+      'name6',
+      'name7',
+      'name8',
+      'name9',
+      'name10',
+      'name11',
     ];
 
-    const reminder = this.listForTest.length % this.columnAmount;
-    const fillerCount = reminder === 0 ? 0 : this.columnAmount - reminder;
-    this.fillers = Array(fillerCount).fill(null);
+    this.chunkedList = this.chunkArray(this.listForTest, this.columnAmount);
+    console.log(this.chunkedList);
+  }
+
+  ngAfterContentInit(): void {
+    this.bookService.getBooks().subscribe({
+      next(value) {
+        console.log(value);
+      },
+    });
+  }
+
+  private chunkArray(array: string[], columns: number): any[][] {
+    const result = [];
+
+    const fullList = [...array, 'add'];
+
+    for (let i = 0; i < fullList.length; i += columns) {
+      result.push(fullList.slice(i, i + columns));
+    }
+
+    return result;
+  }
+
+  openUpdModal(item: string) {
+    console.log(item);
+    // this.modal.itemName.set(item);
+    this.dialog.open(BookUpdModalComponent, { width: '300px' });
+  }
+
+  openAddModal(item: string) {
+    console.log(item);
+    // this.modal.itemName.set(item);
+    this.dialog.open(AddBookModalComponent, { width: '300px' });
   }
 }
