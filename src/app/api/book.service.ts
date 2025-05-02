ebb1f5b6bccd0232aa4cccc012e09084bb8fe775
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-02 11:37:32 +0300

    update book service

--- src/app/api/book.service.ts
+++ src/app/api/book.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { accessToken } from './helper';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -14,28 +14,125 @@ export class BookService {
 
   constructor() {}
 
+  books = signal<Book[]>([]);
+
+  isLoading = signal(false);
+
   token: string | undefined = accessToken();
 
-  getBooks(): Observable<Book[]> {
+  getBooks() {
+    this.isLoading.set(true);
+
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${this.token}`,
     };
 
-    return this.http.get<Book[]>(`${this.baseURL}`, { headers });
+    this.http.get<Book[]>(`${this.baseURL}`, { headers }).subscribe({
+      next: (books) => {
+        this.books.set(books);
+        this.isLoading.set(false);
+      },
+      error: (error) => {
+        console.error('Failed to fetch books:', error);
+        this.isLoading.set(false);
+      },
+    });
   }
 
-  postBook(dto: AddBook): Observable<Book> {
+  // postBook(dto: AddBook | unknown): Observable<Book> {
+  postBook(dto: AddBook | unknown) {
+    const headers = {
+      // // 'Content-Type': 'application/json',
+      // 'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${this.token}`,
+    };
+
+    this.http.post<Book>(`${this.baseURL}`, dto, { headers }).subscribe({
+      next: (v) => {
+        const oldBooks = this.books();
+        this.books.set([...oldBooks, v]);
+      },
+    });
+  }
+
+  // updateBook(dto: AddBook | unknown, bookId: string): Observable<Book> {
+  updateBook(dto: AddBook | unknown, bookId: string) {
+    this.isLoading.set(true);
+
+    const headers = {
+      Authorization: `Bearer ${this.token}`,
+    };
+
+    this.http
+      .put<Book>(`${this.baseURL}/${bookId}`, dto, { headers })
+      .subscribe({
+        next: (book) => {
+          const oldBooks = this.books();
+
+          const updBookIndex = oldBooks.findIndex((b) => b._id === book._id);
+
+          if (updBookIndex !== -1) {
+            const newArr = [...oldBooks];
+            newArr.splice(updBookIndex, 1, book);
+            this.books.set(newArr);
+            this.isLoading.set(false);
+          }
+        },
+      });
+  }
+
+  // favoriteBook(bookId: string): Observable<Book> {
+  favoriteBook(bookId: string) {
     const headers = {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${this.token}`,
     };
 
-    return this.http.post<Book>(`${this.baseURL}`, dto, { headers });
+    this.http
+      .patch<Book>(
+        `${this.baseURL}/${bookId}/favorite`,
+        {},
+        {
+          headers,
+        }
+      )
+      .subscribe({
+        next: (value) => {
+          const oldBooks = this.books();
+
+          const changedBookStatusIndex = oldBooks.findIndex(
+            (book) => book._id === value._id
+          );
+
+          if (changedBookStatusIndex !== -1) {
+            const newArr = [...oldBooks];
+            newArr.splice(changedBookStatusIndex, 1, value);
+            this.books.set(newArr);
+          }
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
-  checkIfToken(): void {
-    console.log(this.token);
-    return;
+  deleteBook(bookId: string) {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    };
+
+    this.http.delete<Book>(`${this.baseURL}/${bookId}`, { headers }).subscribe({
+      next: (book) => {
+        if (!book) throw new Error('No user!');
+        const fullList = this.books();
+        const newList = fullList.filter((_book) => _book._id !== book._id);
+        this.books.set(newList);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 }
