d602715ec3ae42818fb078be0c3c7e7e1a2339c1
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-02 11:38:49 +0300

    ai chat

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,48 @@
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
+import { AiService } from '../api';
+import { BookService } from '../api/book.service';
+import { Book } from '../dtos';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chat',
+  imports: [CommonModule],
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.css',
+})
+export class ChatComponent implements OnInit {
+  books: Book[] = [];
+  answers = signal<string[]>([]);
+  favoriteBooks = signal<Book[]>([]);
+
+  isDisabled = computed(() => this.favoriteBooks().length === 0);
+
+  isLoading = signal(false);
+
+  constructor(private AIService: AiService, private bookService: BookService) {
+    effect(() => {
+      this.books = this.bookService.books();
+      this.favoriteBooks.set(
+        this.books.filter((book) => book.favorite === true)
+      );
+      this.answers.set(this.AIService.answers());
+      this.isLoading.set(this.AIService.isLoading());
+    });
+  }
+
+  ngOnInit(): void {
+    this.AIService.fetchAnswers();
+  }
+
+  onAsk() {
+    try {
+      const booksTitle: string[] = [];
+      this.favoriteBooks().forEach((book) => {
+        booksTitle.push(book.title);
+      });
+      this.AIService.getRec(booksTitle);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
