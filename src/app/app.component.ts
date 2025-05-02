ed6156c13797aca8dceea60ea7d002f77c6fa298
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-02 11:34:43 +0300

    general view of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { ShelfComponent } from './shelf/shelf.component';
 import { FavShelfComponent } from './fav-shelf/fav-shelf.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
+import { ChatComponent } from './chat/chat.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { UserPanelComponent } from './user-panel/user-panel.component';
     ShelfComponent,
     FavShelfComponent,
     UserPanelComponent,
+    ChatComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
