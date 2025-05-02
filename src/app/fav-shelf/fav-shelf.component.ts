77435a8a4304a345f9531e0d791f56dc119a8d84
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-02 11:39:10 +0300

    logic and view of favorite shelf

--- src/app/fav-shelf/fav-shelf.component.ts
+++ src/app/fav-shelf/fav-shelf.component.ts
@@ -1,86 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, effect, OnInit, signal } from '@angular/core';
+import { Book } from '../dtos';
+import { BookService } from '../api/book.service';
+import { MatDialog } from '@angular/material/dialog';
+import { BookComponent } from '../book-component/book-component.component';
 
 @Component({
   selector: 'app-fav-shelf',
-  imports: [],
+  imports: [BookComponent],
   templateUrl: './fav-shelf.component.html',
   styleUrl: './fav-shelf.component.css',
 })
-export class FavShelfComponent implements OnInit {
-  public listForTest: string[] = [];
+export class FavShelfComponent {
+  public listOfBooks: Book[] = [];
+  public listOfFavBooks: Book[] = [];
+  public chunkedList: Book[][] = [];
   private columnAmount = 7;
-  public fillers: any[] = [];
 
-  ngOnInit(): void {
-    this.listForTest = [
-      // 'name1',
-      // 'name2',
-      // 'name3',
-      // 'name4',
-      // 'name5',
-      // 'name6',
-      // 'name7',
-      // 'name8',
-      // 'name9',
-      // 'name10',
-      // 'name11',
-      // 'name21',
-      // 'name31',
-      // 'name41',
-      // 'name51',
-      // 'name61',
-      // 'name71',
-      // 'name81',
-      // 'name91',
-      // 'name101',
-      // 'name12',
-      // 'name22',
-      // 'name32',
-      // 'name42',
-      // 'name52',
-      // 'name62',
-      // 'name72',
-      // 'name82',
-      // 'name92',
-      // 'name102',
-      // 'name112',
-      // 'name212',
-      // 'name312',
-      // 'name412',
-      // 'name512',
-      // 'name612',
-      // 'name712',
-      // 'name812',
-      // 'name912',
-      // 'name1012',
-    ];
+  constructor(private bookService: BookService, public dialog: MatDialog) {
+    effect(() => {
+      const books = this.bookService.books();
+      this.listOfBooks = books;
+      this.listOfFavBooks = this.listOfBooks.filter(
+        (book) => book.favorite === true
+      );
+      this.chunkedList = this.chunkArray(this.listOfBooks, this.columnAmount);
+    });
+  }
 
-    const reminder = this.listForTest.length % this.columnAmount;
-    const fillerCount = reminder === 0 ? 0 : this.columnAmount - reminder;
-    this.fillers = Array(fillerCount).fill(null);
+  private chunkArray(array: Book[], columns: number): any {
+    const result = [];
+    const favList = [];
+    try {
+      for (let i = 0; i < array.length; i++) {
+        if (array[i].favorite) favList.push(array[i]);
+      }
 
-    // const observable = new Observable(function subscribe(subscriber) {
-    //   try {
-    //     subscriber.next(1);
-    //     subscriber.next(2);
-    //     subscriber.next(3);
-    //     subscriber.complete();
-    //   } catch (err) {
-    //     subscriber.error(err); // delivers an error if it caught one
-    //   }
-    // });
+      for (let i = 0; i < favList.length; i += columns) {
+        result.push(favList.slice(i, i + columns));
+      }
+    } catch (error) {}
 
-    // observable.subscribe({
-    //   next(x) {
-    //     console.log('x: ', x);
-    //   },
-    //   error(err) {
-    //     console.error(err);
-    //   },
-    //   complete() {
-    //     console.log('Done!');
-    //   },
-    // });
+    return result;
   }
 }
