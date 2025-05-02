b13e188d10598745c2575596191d8aad69e0ce40
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-02 11:37:09 +0300

    add ai service

---
+++ src/app/api/ai.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { accessToken as at } from './helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AiService {
+  private URL = 'http://localhost:3500/ai';
+
+  private http = inject(HttpClient);
+
+  answers = signal<string[]>([]);
+  reqErrMessage = signal<string>('');
+
+  isLoading = signal(false);
+
+  token = at();
+
+  fetchAnswers() {
+    this.isLoading.set(true);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+
+    this.http.get<string[]>(this.URL, { headers }).subscribe({
+      next: (answers) => {
+        this.answers.set(answers);
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.error(err);
+        this.reqErrMessage.set(err.error.message);
+        this.isLoading.set(false);
+      },
+    });
+  }
+
+  getRec(booksTitle?: string[]) {
+    this.isLoading.set(true);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+    if (booksTitle) {
+      this.http
+        .post<{ result: string }>(`${this.URL}/recomendations`, booksTitle, {
+          headers,
+        })
+        .subscribe({
+          next: (ans: { result: string }) => {
+            const newList = [...this.answers(), ans.result];
+            this.answers.set(newList);
+            this.isLoading.set(false);
+          },
+          error: (err) => {
+            this.reqErrMessage.set(err.error.message || 'An error occurred');
+            this.isLoading.set(false);
+          },
+        });
+    }
+  }
+}
